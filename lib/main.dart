670a0bfcd9d13873da7df97ab6cc49355b664876
Dismhndraa <2306093@itg.ac.id>
2026-03-12 20:43:22 +0700

    fitur update

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,176 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(HomePage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    String title = 'Journey of Ours';
-    String name = 'Dimas Hendra Yudha dan Deby Fatika Saputri';
-    String message = "Halo nama kami $name, selamat datang di $title";
-
+class HomePage extends StatelessWidget {
+  build(context) {
     return MaterialApp(
-      title: title,
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.deepOrange,
-          title: Text(title),
+          backgroundColor: Colors.blue,
+          leading: Icon(Icons.home),
+          title: Text('Aplikasi Pertamaku'),
         ),
-        body: Center(
-          child: Text(message),
+        body: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: .start,
+              children: [
+                Row(children: [Icon(Icons.archive), Text("Artikel Baru")]),
+                Center(
+                  child: Card(
+                    child: Column(
+                      crossAxisAlignment: .center,
+                      mainAxisAlignment: .center,
+                      children: [
+                        Image.network(
+                          'https://picsum.photos/seed/picsum/200/300',
+                        ),
+                        Text(
+                          "5 Rekomendasi Wisata di Islandia",
+                          style: TextStyle(fontWeight: .bold),
+                        ),
+                        Text(
+                          "Islandia merupakan negara yang berada di Eropa, dimana negara ini menyuguhkan beberapa wisata alam yang menenangkan.",
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Icon(Icons.chat),
+                    Text(
+                      'Komentar',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                ListView(
+                  shrinkWrap: true,
+                  children: [
+                    Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(3),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("DimasNathafhilano"),
+                            Text("Tiris pisan jiga di Pangalengan"),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(3),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Deby Fatika Saputri"),
+                            Text(
+                              "Pemandangan alam nya bagus banget, jadi pengen kesini lagi",
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(3),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Finn"),
+                            Text(
+                              "Kira-kira butuh budget berapa ya buat kesana?",
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(3),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Yurisa"),
+                            Text("Harus cobain Hangikjöt deh, itu enak banget"),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(3),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [Text("Windah"), Text("Bawah Gue Ganteng")],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(3),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [Text("Windah"), Text("Makasih bang")],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(3),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Prawira"),
+                            Text(
+                              "Impianku kesana pengen makan salju campur marjan",
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(3),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Travelindo"),
+                            Text("Open trip Islandia 7 hari cuman 19 juta aja"),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(3),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Lenathea"),
+                            Text(
+                              "Tempatnya bagus sih tapi orang-orangnya jutek",
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
